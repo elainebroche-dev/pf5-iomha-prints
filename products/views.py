e170348c210dc013b3df1a23741a0c1e8beb911f
elainebroche-dev <elainebroche@gmail.com>
2021-11-11 20:14:40 +0000

    Add search of artist and title to support User Story US304

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Print, PrintOption
 
 # Create your views here.
@@ -9,9 +11,21 @@ def all_products(request):
     """ View to show all print products, incl sorting and search queries """
 
     products = Print.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(title__icontains=query) | Q(artist__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
