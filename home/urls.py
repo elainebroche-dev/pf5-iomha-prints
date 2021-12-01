743b1c1da7aeb623880713b212759dccc95bebb6
elainebroche-dev <elainebroche@gmail.com>
2021-12-01 10:46:56 +0000

    US604 - add GDPR page and tidy up footer

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy')
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def privacy_policy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy_policy.html')
