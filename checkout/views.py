95d0ebc7ba313ba969651cf04722f4b3c2610d13
elainebroche-dev <elainebroche@gmail.com>
2021-12-06 22:19:41 +0000

    Fix bug where profile DOB gets overwritten

--- checkout/views.py
+++ checkout/views.py
@@ -159,6 +159,7 @@ def checkout_success(request, order_number):
                 'default_street_address1': order.street_address1,
                 'default_street_address2': order.street_address2,
                 'default_county': order.county,
+                'dob': order.user_profile.dob,  #ebr
             }
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
